63e593d087f2536501b12bdc0e777b9092eb4a2d
sabri-amine <sabriamine@gmail.com>
2024-10-30 10:18:41 +0100

    ajouter counteur dans tache global todo

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,7 @@ submitTache.addEventListener("click", function(e) {
     tasks.push(task);
     localStorage.setItem('taskss', JSON.stringify(tasks));
 
-    // Clear the input fields
+   
     document.getElementById("title").value = "";
     document.getElementById("description").value = "";
     document.getElementById("date").value = "";
@@ -40,7 +40,17 @@ submitTache.addEventListener("click", function(e) {
     document.getElementById("Statut").value = "";
 
     ajouteTask();
+    
 });
+
+function TaskCount(){
+    const count = document.getElementById("count");
+    let tasks = JSON.parse(localStorage.getItem('taskss')) || [];
+    const taskCount = tasks.length;
+
+    count.innerText = taskCount
+    // console.log(taskCount);
+}
 
 window.onload = function() {
     ajouteTask();
@@ -49,10 +59,9 @@ window.onload = function() {
 function ajouteTask() {
     const tasks = JSON.parse(localStorage.getItem('taskss')) || [];
     const todo = document.getElementById("tache");
-    todo.innerHTML = ""; // Clear existing tasks
-
+    todo.innerHTML = ""; 
     tasks.forEach((task, index) => {
-        const divTask = document.createElement('div'); // Create a new div for each task
+        const divTask = document.createElement('div'); 
         divTask.innerHTML = `
             <div class="header-tache border-b-2 border-zinc-900 flex justify-between items-center w-full p-3">
                 <i class="bi bi-pencil-square"></i>
@@ -63,17 +72,24 @@ function ajouteTask() {
                 <p>${task.description}</p>
             </div>
             <div class="foot-tache flex justify-between mt-2">
-                <p class="border-2 border-zinc-900 w-24 text-center font-bold rounded-se-xl">${task.priority}</p>
+                <p class="border-2 border-zinc-900 w-24 text-center font-bold rounded-se-xl 
+                ${task.priority === 'p1' ? 'bg-red-500' : 
+                    task.priority === 'p2' ? 'bg-green-500' : 
+                    task.priority === 'p3' ? 'bg-blue-500' : 'bg-lime-500'} 
+                text-neutral-50">
+                ${task.priority}
+                </p>
                 <input type="date" class="border-2 border-zinc-900 w-34 text-center font-bold rounded-es-xl" value="${task.date}">
             </div>
         `;
 
         divTask.querySelector('.bi-trash').addEventListener('click', function() {
-            deleteTask(index); // Pass the current index to deleteTask
+            deleteTask(index); 
         });
 
         todo.appendChild(divTask);
     });
+    TaskCount();
 }
 
 function deleteTask(index) {
