f4d4f6c35f1b76a25bf15cf4d925ae21f9fea0bf
sabri-amine <sabriamine@gmail.com>
2024-10-31 12:32:56 +0100

    ajouter responsive dans package.json et ajouter petit modification dans fichier tailwind confing

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px'
+    }
   },
   plugins: [],
 }
-
